be3e2d3f984bd7ad3576ef160c6d10831ba4b05e
yasuhiro.kato <yasuhiro.kato@cct-inc.co.jp>
2019-03-22 11:27:35 +0900

    Add no756

---
+++ README.md
@@ -0,0 +1,8 @@
+# yukicoder
+
+[yukicoder](https://yukicoder.me/)の解答コードリポジトリ
+
+## 目的
+
+- アルゴリズムの基礎力向上のため
+- Go言語の基礎力向上のため
---
+++ no46/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scanln(&a, &b)
+
+	if b%a == 0 {
+		fmt.Println(b / a)
+	} else {
+		fmt.Println(b/a + 1)
+	}
+}
